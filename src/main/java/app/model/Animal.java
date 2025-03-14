69409322adde712265877157bb8a1433ca9d23ff
Fillyshka <grairent1@mail.ru>
2025-03-14 13:08:51 +0300

    Initial commit

---
+++ src/main/java/app/model/Animal.java
@@ -0,0 +1,10 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public abstract class Animal {
+}
